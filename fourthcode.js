70849c36481a67342adceefee6824a397faa5488
Javed <nadafjaved92@gmail.com>
2025-08-10 20:17:14 +0530

    my practice code

---
+++ fourthcode.js
@@ -0,0 +1,16 @@
+// let num = 1
+// while(num<=10){
+//     console.log(`5*${num}=${5*num}`)
+//     num++
+// }
+
+// i=10
+// while(i>=1){
+//     console.log(i)
+//     i--
+// }
+i=10
+while(i>=1){
+    console.log(i)
+    i=i-1
+}
---
+++ pattern.js.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+// # *
+// # * *
+// # * * *
+// # * * * *
+// # * * * * *
+
+
+// for(let i=1; i<6; i++){
+//     console.log("* ".repeat(i))
+// }
+
+
+for(let i=1; i<6; i++){
+    console.log("* ".repeat(i))
+}
